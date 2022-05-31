e7634b00c099bcb711b91ef70d5bf2059ce1b5c3
Ryuji Oda <ryuji.oda@gmail.com>
2022-05-31 10:23:09 +0900

    add: example image atom

--- compose-card-example/app/src/main/java/com/example/compose_card_example/MainActivity.kt
+++ compose-card-example/app/src/main/java/com/example/compose_card_example/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.compose_card_example.ui.components.atoms.ProfileImage
 import com.example.compose_card_example.ui.theme.ComposecardexampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ProfileImage(url = null)
                 }
             }
         }
---
+++ compose-card-example/app/src/main/java/com/example/compose_card_example/ui/components/atoms/ProfileImage.kt
@@ -0,0 +1,33 @@
+package com.example.compose_card_example.ui.components.atoms
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
+
+@Composable
+fun ProfileImage(url: String?) {
+    Image(
+        painter = rememberAsyncImagePainter(url ?: "https://user-images.githubusercontent.com/59087018/171071634-8c67e0a3-3e1c-4108-b63e-eb7616c5672f.jpg"),
+        contentDescription = "profile image",
+        modifier = Modifier
+            .size(128.dp)
+            .clip(CircleShape)
+            .border(2.dp, Color.Gray, CircleShape),
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewImage() {
+    ProfileImage(url = "https://user-images.githubusercontent.com/59087018/171071634-8c67e0a3-3e1c-4108-b63e-eb7616c5672f.jpg")
+}
