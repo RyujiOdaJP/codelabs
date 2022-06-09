13fb3d76c27dc74ec855d2bb6e09cde659e5d2a4
Ryuji Oda <ryuji.oda@gmail.com>
2022-06-09 23:52:29 +0900

    wip

--- compose-card-example/app/src/main/java/com/example/compose_card_example/MainActivity.kt
+++ compose-card-example/app/src/main/java/com/example/compose_card_example/MainActivity.kt
@@ -1,29 +1,59 @@
 package com.example.compose_card_example
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose_card_example.ui.components.atoms.ProfileImage
+import com.example.compose_card_example.ui.components.molecules.InfoRow
 import com.example.compose_card_example.ui.theme.ComposecardexampleTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             ComposecardexampleTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ProfileImage(url = null)
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.spacedBy(16.dp),
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        ProfileImage(url = null)
+
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.SpaceAround
+                        ) {
+                            InfoRow(
+                                icon = android.R.drawable.ic_menu_call,
+                                text = "080 9999 0093",
+                                contentDescription = "TEL"
+                            )
+                            InfoRow(
+                                icon = android.R.drawable.ic_menu_call,
+                                text = "080 9999 0093",
+                                contentDescription = "TEL"
+                            )
+                        }
+                    }
                 }
             }
         }
--- compose-card-example/app/src/main/java/com/example/compose_card_example/ui/components/molecules/InfoRow.kt
+++ compose-card-example/app/src/main/java/com/example/compose_card_example/ui/components/molecules/InfoRow.kt
@@ -1,4 +1,43 @@
 package com.example.compose_card_example.ui.components.molecules
 
-class InfoRow {
+import android.os.Build
+import androidx.annotation.DrawableRes
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@RequiresApi(Build.VERSION_CODES.P)
+@Composable
+fun InfoRow(@DrawableRes icon: Int, text: String, contentDescription: String) {
+    Row(
+        modifier = Modifier
+            .padding(horizontal = 16.dp, vertical = 4.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(id = icon),
+            contentDescription = contentDescription,
+        modifier = Modifier.padding(end = 44.dp)
+        )
+        Text(text = text)
+    }
+}
+
+@Composable
+@Preview
+fun PreviewInfoRow() {
+    InfoRow(
+        icon = android.R.drawable.ic_menu_call,
+        text = "+81 (080) 8111 0333",
+        contentDescription = "Tel"
+    )
 }
